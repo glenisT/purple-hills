3458e2dbbd07b50b398572ead48856e7c4ed86e4
g.tivari <g.tivari@sogetel.it>
2022-01-10 18:05:51 +0100

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './home/header/header.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { HomepageComponent } from './home/homepage/homepage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    HomepageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
